a9b6aa34e83ef9976758e97aed258640b39f8028
unknown <nkinbox@outlook.com>
2025-06-22 01:06:47 +0530

    wip

--- test_weaviate.py
+++ test_weaviate.py
@@ -1,5 +1,5 @@
 import weaviate
-
+from weaviate.classes.aggregate import GroupByAggregate
 # Connect to local Weaviate instance
 client = weaviate.connect_to_local()
 
@@ -13,6 +13,9 @@ collections = client.collections.list_all()
 for name in collections:
     collection = client.collections.get(name)
     count = collection.aggregate.over_all()
-    print(f"Collection: {name}, Object Count: {count}")
+    print(f"Collection: {name}, Object Count: {count.total_count}")
+    groupBy = collection.aggregate.over_all(group_by=GroupByAggregate(prop="file_id"))
+    for group in groupBy.groups:
+        print(f"\tfile: {group.grouped_by.value} Count: {group.total_count}")
 
 client.close()
